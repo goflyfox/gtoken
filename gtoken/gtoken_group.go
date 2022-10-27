1084b66a9443c5343202aa2adc18ee5ca7913e23
cncowboy <cncowboy@qq.com>
2022-10-27 17:57:30 +0800

    一套配置，支持多种登录方式：用户名密码，短信...

--- gtoken/gtoken_group.go
+++ gtoken/gtoken_group.go
@@ -22,10 +22,20 @@ func (m *GfToken) Middleware(group *ghttp.RouterGroup) error {
 		return errors.New("CacheMode set error")
 	}
 	// 登录
-	if m.LoginPath == "" || m.LoginBeforeFunc == nil {
-		g.Log().Error("[GToken]LoginPath or LoginBeforeFunc not set")
-		return errors.New("LoginPath or LoginBeforeFunc not set")
-	}
+	// if m.LoginPath == "" || m.LoginBeforeFunc == nil {
+	//	g.Log().Error("[GToken]LoginPath or LoginBeforeFunc not set")
+	//	return errors.New("LoginPath or LoginBeforeFunc not set")
+	// }
+	if m.LoginPathMap == nil {
+                m.LoginPathMap = make(map[string]LoginBeforeFunction)
+        }
+        if m.LoginPath != "" && m.LoginBeforeFunc != nil {
+                m.LoginPathMap[m.LoginPath] = m.LoginBeforeFunc
+        }
+	if m.LoginPathMap == nil || len(m.LoginPathMap) == 0 {
+		g.Log().Error("[GToken]LoginPath or LoginBeforeFunc or LoginPathMap not set")
+		return errors.New("LoginPath or LoginBeforeFunc or LoginPathMap not set")
+        }
 	// 登出
 	if m.LogoutPath == "" {
 		g.Log().Error("[GToken]LogoutPath not set")
@@ -33,7 +43,10 @@ func (m *GfToken) Middleware(group *ghttp.RouterGroup) error {
 	}
 
 	group.Middleware(m.authMiddleware)
-	group.ALL(m.LoginPath, m.Login)
+	// group.ALL(m.LoginPath, m.Login)
+	for loginPath, _ := range m.LoginPathMap {
+		group.ALL(loginPath, m.Login)
+        }
 	group.ALL(m.LogoutPath, m.Logout)
 
 	return nil
